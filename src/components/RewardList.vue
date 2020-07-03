252cd3d014dd6201ce2a5bd556b3849935310a88
kgoel085 <kgoel085@gmail.com>
2020-07-03 11:38:36 +0530

    Make the output dynamic accor to input

--- src/components/RewardList.vue
+++ src/components/RewardList.vue
@@ -17,6 +17,10 @@
               <td>{{material.base - material.subtract}}</td>
               <td>{{(material.base + 4) - material.subtract}}</td>
             </tr>
+            <tr>
+              <td>Conversion Kits</td>
+              <td colspan="2">{{ getRewardKits }}</td>
+            </tr>
           </tbody>
         </template>
       </v-simple-table>
@@ -27,7 +31,9 @@
 <script>
 import { mapGetters } from 'vuex'
 export default {
-  data: () => ({}),
+  data: () => ({
+    rewardKits: 'N/A'
+  }),
   computed: {
     ...mapGetters({
       mData: 'Material/getData', // Material Data
@@ -35,12 +41,16 @@ export default {
     }),
     rewardListArr () {
       const returnVal = []
+      this.eventRewardInputArr.map(obj => obj.value)
       for (const material of this.mData) {
         const finalObj = this.getActualRewardCount(material)
         returnVal.push(finalObj)
       }
 
       return returnVal
+    },
+    getRewardKits () {
+      return this.rewardKits
     }
   },
   methods: {
@@ -53,75 +63,106 @@ export default {
         case 'Flywheel': {
           if (selArr[0] === 'Flywheel') material.subtract = 4
           else if (selArr[0] === 'Thrust Sleeve') material.subtract = 6
+          else material.subtract = 0
           break
         }
 
         case 'Thrust Sleeve': {
           if (selArr[0] === 'Thrust Sleeve') material.subtract = 4
           else if (selArr[0] === 'Flywheel') material.subtract = 6
+          else material.subtract = 0
           break
         }
 
         case 'Input Shaft': {
           if (selArr[1] === 'Input Shaft' || selArr[1] === 'Tread') material.subtract = 5
+          else material.subtract = 0
           break
         }
 
         case 'Tread': {
           if (selArr[1] === 'Input Shaft' || selArr[1] === 'Tread') material.subtract = 5
+          else material.subtract = 0
           break
         }
 
         case 'Thermosensor': {
           if (selArr[4] === 'Thermosensor') material.subtract = 7
           else if (selArr[4] === 'Valve') material.subtract = 5
+          else material.subtract = 0
           break
         }
 
         case 'Valve': {
           if (selArr[4] === 'Valve') material.subtract = 7
           else if (selArr[4] === 'Thermosensor') material.subtract = 5
+          else material.subtract = 0
           break
         }
 
         case 'Intake': {
           if (selArr[5] === 'Intake') material.subtract = 2
           else if (selArr[5] === 'Backplate') material.subtract = 6
+          else material.subtract = 0
           break
         }
 
         case 'Backplate': {
           if (selArr[5] === 'Backplate') material.subtract = 2
           else if (selArr[5] === 'Intake') material.subtract = 6
+          else material.subtract = 0
           break
         }
 
         case 'Clutch Plate': {
           if (selArr[12] === 'Clutch Plate') material.subtract = 5
           else if (selArr[12] === 'Brake Disc') material.subtract = 4
+          else material.subtract = 0
           break
         }
 
         case 'Brake Disc': {
           if (selArr[12] === 'Brake Disc') material.subtract = 5
           else if (selArr[12] === 'Clutch Plate') material.subtract = 4
+          else material.subtract = 0
           break
         }
 
         case 'Harness': {
           if (selArr[8] === 'Harness') material.subtract = 5
           else if (selArr[8] === 'Tank') material.subtract = 4
+          else material.subtract = 0
           break
         }
 
         case 'Tank': {
           if (selArr[8] === 'Tank') material.subtract = 5
           else if (selArr[8] === 'Harness') material.subtract = 4
+          else material.subtract = 0
           break
         }
       }
 
+      this.getConversionKitCount()
       return material
+    },
+
+    // Calculate conversion kits
+    getConversionKitCount () {
+      const inputArr = this.eventRewardInputArr.map(obj => obj.value)
+      const sel1 = (inputArr[61] === 'Gearbox Upgrade Kit 1' && inputArr[64] === 'Nitro Upgrade Kit 1') ? 1 : 0
+      const sel2 = (inputArr[61] === 'Wheel Upgrade Kit 1' && inputArr[64] === 'Nitro Upgrade Kit 1') ? 2 : 0
+      const sel3 = (inputArr[61] === 'Wheel Upgrade Kit 1' && inputArr[64] === 'ECU Upgrade Kit 1') ? 3 : 0
+      const sel4 = (inputArr[61] === 'Gearbox Upgrade Kit 1' && inputArr[64] === 'ECU Upgrade Kit 1') ? 4 : 0
+
+      let returnVal = 'N/A'
+      if (sel1 === 1) returnVal = 'Engine, Turbo, Wheel & ECU Conversion Kits'
+      else if (sel2 === 2) returnVal = 'Engine, Turbo, Gearbox & ECU Conversion Kits'
+      else if (sel3 === 3) returnVal = 'Engine, Turbo, Gearbox & Nitro Conversion Kits'
+      else if (sel4 === 4) returnVal = 'Engine, Turbo, Wheel & Nitro Conversion Kits'
+
+      this.rewardKits = returnVal
+      return returnVal
     }
   }
 }
